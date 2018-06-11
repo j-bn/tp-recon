83063b96ca15ffbbf8bd999dcaae1ee0946a288d
James Thornton <jamesthornton78@gmail.com>
2018-06-11 19:21:03 +0100

    Comment

--- fcinterface/dronekit_interface.py
+++ fcinterface/dronekit_interface.py
@@ -65,7 +65,7 @@ class FCInterface:
 				# except:
 				# 	pass
 
-			elif read.contains('COMMAND'): 	# first line so will begin with ...>
+			elif read.contains('COMMAND'): 	# first line so will begin with ...> meaning startswith doesnt work
 				stackHeight += 1
 
 			elif read.startswith('DONE'):
