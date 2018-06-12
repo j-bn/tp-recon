9f9c2ea83cda81e9ed8df06a10b61c27cf03e370
Team Peryton RPi <teamperyton@surrey.ac.uk>
2018-06-12 11:04:33 +0000

    tracked files fixed

---
+++ boot.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# auto-boot is achieved in /etc/rc.local
+
+cd /home/pi/tp-recon/
+
+# confirmed that this script runs at boot with tp-recon as the working directory
+#echo "running tp-recon/boot.sh" > /tmp/tp_test.txt
+#echo "running tp-recon/boot.sh" > boot_test.txt
+
+#sudo lxterminal -e python3 daemon.py
+python3 daemon.py
